bce3c547ec2fb1ebff0aebfe1591eaedf28f9557
Chamila Amithirigala <chamila@wirelessprvnat-172-17-37-103.near.illinois.edu>
2015-10-29 15:07:04 -0500

    major stats

---
+++ swe-stats.py
@@ -0,0 +1,88 @@
+import subprocess
+import os
+from collections import defaultdict
+
+def extract_major(students):
+	majors = defaultdict(int)
+	for student in students:
+		major = student['student_major_name']
+		majors[major] += 1
+	return majors
+
+def filters(results):
+	final_results = []
+	for result in results:
+		if 'student_major_name' not in result: 
+			continue
+		if 'student_level_description' not in result:
+			continue
+		if 'undergrad' not in result['student_level_description']:
+			continue
+		final_results.append(result)
+	if len(final_results) < 1:
+		return {'student_major_name': None}
+	else:
+		return final_results[-1]
+
+def nph_query(firstname, lastname):
+	command = ['nph', firstname, lastname]
+	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err = process.communicate()
+	if 'no matches to query' in out:
+		command = ['nph', lastname]
+		process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = process.communicate()
+
+	out = out.split('\n')
+	out = [each.lower() for each in out if ':'in each]
+	return out
+
+def get_nph(names):
+	students = []
+	for (firstname, lastname) in names:
+		out = nph_query(firstname, lastname)
+
+		curr_index = -1
+		results = []
+		for line in out:
+			if 'entry' in line:
+				curr_index += 1
+				results.append({})
+				continue
+			if curr_index < 0:
+				continue
+			if ':' in line:
+				text = line.split(':')
+				if len(text) < 2: continue
+				key = text[0].strip()
+				value = text[1].strip()
+				results[curr_index][key] = value
+
+		results = filters(results)
+
+		students.append(results)
+
+	return students
+
+def parseMembers(filename):
+	with open(filename, 'rb') as f:
+		lines = f.readlines()
+
+	names = []
+	for line in lines[1:]:
+		firstname = line[0].lower()
+		lastname = line[1].lower()
+		names.append(' '.join([firstname, lastname]))
+
+	return names
+
+def get_statistics(filename):
+	names = parseMembers(filename)
+	students = get_nph(names)
+	majors = get_majors(students)
+	total = sum(majors.values())
+	for major in majors:
+		print major, float(majors[major])/total
+
+if __name__ == '__main__':
+	get_majors('nat-mem.txt')
